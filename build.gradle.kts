51312d75b1984e9624aa0c602065e5bc235ef57a
Mateusz Bajdak <mateuszbajdak@gmail.com>
2019-08-29 21:48:52 +0200

    encrypt password

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,16 +17,16 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo")
-	testImplementation("io.projectreactor:reactor-test")
+    compile("com.github.mowedgrass:jasypt-gradle-boot-plugin:0.1.33")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo")
+    testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.spockframework:spock-spring:1.3-groovy-2.5")
-
 }
 
 tasks.withType<KotlinCompile> {
