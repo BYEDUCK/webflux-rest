b7aa7bd274c73ceeda0b9a90cfd6f0fa69c25778
Mateusz Bajdak <mateuszbajdak@gmail.com>
2019-08-31 20:40:44 +0200

    Added scheduled db counters in order to keep db connection alive.

---
+++ src/main/kotlin/com/byeduck/webfluxrest/DatabaseScheduledCounter.kt
@@ -0,0 +1,30 @@
+package com.byeduck.webfluxrest
+
+import com.byeduck.webfluxrest.repositories.CategoryRepository
+import com.byeduck.webfluxrest.repositories.VendorRepository
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.scheduling.annotation.Async
+import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+
+@EnableAsync
+@Component
+class DatabaseScheduledCounter(
+        @Autowired private val categoryRepository: CategoryRepository,
+        @Autowired private val vendorRepository: VendorRepository
+) {
+
+    private val logger: Logger = LoggerFactory.getLogger(DatabaseScheduledCounter::class.java)
+
+    @Async
+    @Scheduled(fixedDelay = 20000, initialDelay = 15000)
+    fun countCategories() = logger.info("Categories count: {}", categoryRepository.count().block())
+
+    @Async
+    @Scheduled(fixedDelay = 20000, initialDelay = 5000)
+    fun countVendors() = logger.info("Vendors count: {}", vendorRepository.count().block())
+
+}
--- src/main/kotlin/com/byeduck/webfluxrest/WebfluxRestApplication.kt
+++ src/main/kotlin/com/byeduck/webfluxrest/WebfluxRestApplication.kt
@@ -3,8 +3,10 @@ package com.byeduck.webfluxrest
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @EnableEncryptableProperties
+@EnableScheduling
 @SpringBootApplication
 class WebfluxRestApplication
 
